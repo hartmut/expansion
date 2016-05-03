ac6cff4f4c6a0a0482cd1fa3d50a3add4d81ecaa
Hartmut Prochaska <hartmut.prochaska@gmail.com>
2016-05-03 21:57:30 +0200

    small steps get you somewhere

--- server/src/common/ticker.rs
+++ server/src/common/ticker.rs
@@ -3,3 +3,12 @@
 // See doc/LICENSE for licensing information
 //
 // central worker, using for ticks in the "universe"
+
+
+use time::{Duration, PreciseTime};
+
+let start = PreciseTime::now();
+
+while start.to(PreciseTime::now()) < Duration::seconds(1) {
+    //call the different update methods for the next tick
+}
