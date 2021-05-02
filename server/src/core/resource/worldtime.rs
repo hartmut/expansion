704dbdd84eb19c328391c91a2e445f4345e4f5b7
Hartmut Prochaska <hartmut.prochaska@gmail.com>
2021-05-02 12:42:15 +0200

    update worldtime

--- server/src/core/resource/worldtime.rs
+++ server/src/core/resource/worldtime.rs
@@ -4,8 +4,6 @@
 //
 // descriptions for entities
 use chrono;
-use log::info;
-use std::thread;
 use std::time::{Duration, SystemTime};
 
 // Descriptions
@@ -33,41 +31,4 @@ impl Worldtime {
         }
     }
 
-    // COMBAK modify to system
-    pub fn step(&mut self) {
-        info!("this is a step of worldtime {}", self.worldtime);
-
-        //sleep and one more step
-        thread::sleep(self.tick_dur.to_std().unwrap());
-        self.tick_counter += 1;
-
-        //update worldtime
-        let time_now = SystemTime::now();
-        self.step_leng = time_now
-            .duration_since(self.time_last)
-            .expect("SystemTime::duration_since failed");
-        let step_leng_warp = self.step_leng * (self.warp as u32);
-        let chrono_step_leng = chrono::Duration::from_std(step_leng_warp).unwrap();
-        self.worldtime = self.worldtime + chrono_step_leng;
-
-        // save time of this step
-        self.time_last = time_now;
-
-        info!(
-            "step length with warp {:?}",
-            self.step_leng * (self.warp as u32)
-        );
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use specs::prelude::*;
-
-    #[test]
-    fn create_resource_worldtime() {
-        let mut world = specs::World::new();
-        world.insert(Worldtime::new(6, 2));
-    }
 }
