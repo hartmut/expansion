1d23401f0ba7037f439fc4af9f276d6f8c78ca70
Hartmut Prochaska <hartmut.prochaska@gmail.com>
2021-05-28 21:05:28 +0200

    add structures for asteroid import

--- server/src/core/entity/smallbody.rs
+++ server/src/core/entity/smallbody.rs
@@ -11,14 +11,40 @@ pub struct SmallBody;
 * download https://ssd.jpl.nasa.gov/?sb_elem the "numbered Asteroids" list and modify it to a
     comma separated csv file and move it into assets/bodiesorbit.csv
 * enrich it with physical parameters with selection of size > 0.1km
-    into assets/smallbodies.csv. At first select " object fullname" (number and  Name) fto
+    into assets/smallbodies.csv. At first select "object fullname" (number and  Name) to
     have an anchor.
 * more data could propably be found at https://www.asterank.com/
     for example minerals and checmical elements
 */
 
-struct bodies {}
-struct orbits {}
+struct Bodies {
+    full_name: String,
+    diameter: f32,
+    extent: String,
+    albedo: f32,
+    rot_per: f32,
+    gm: f32,
+    bv: f32,
+    ub: f32,
+    ir: String,
+    spec_b: String,
+    spec_t: String,
+}
+
+struct Orbits {
+    num: u32,
+    name: String,
+    epoch: u32,
+    a: f32,
+    e: f32,
+    i: f32,
+    w: f32,
+    node: f32,
+    m: f32,
+    h: f32,
+    g: f32,
+    reference: String,
+}
 
 // TODO implement asteroid import etc.
 impl SmallBody {
