2ba4ac9a1323613f93565a713e458f17403c1858
Hartmut Prochaska <hartmut.prochaska@gmail.com>
2022-02-19 15:38:39 +0100

    cleanup and autosave of objects

--- server/src/utils/mod.rs
+++
@@ -1,7 +0,0 @@
-// Experimental Simulator of a cooperative solar system economy.
-// Copyright (C) 2016  Hartmut Prochaska
-// See doc/LICENSE for licensing information
-//
-// all central tools
-
-pub mod fileoperations;
