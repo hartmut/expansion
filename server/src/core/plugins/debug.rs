0da2fa0c5767193545b143421bf4adba930628b1
Hartmut Prochaska <hartmut.prochaska@gmail.com>
2023-01-04 14:29:41 +0100

    changed gui

--- server/src/core/plugins/debug.rs
+++ server/src/core/plugins/debug.rs
@@ -1,10 +1,14 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
-
+use bevy_editor_pls::prelude::*;
 pub struct MyDebug;
 
 impl Plugin for MyDebug {
     fn build(&self, app: &mut App) {
-        app.add_plugin(WorldInspectorPlugin::new());
+        // either this
+        // app.add_plugin(WorldInspectorPlugin::new());
+
+        // or this
+        app.add_plugin(EditorPlugin);
     }
 }
